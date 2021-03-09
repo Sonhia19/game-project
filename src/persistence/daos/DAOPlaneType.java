f93d1ceaf380c81bab82021a5209cb546732e954
PabloF2922 <44418472+PabloF2922@users.noreply.github.com>
2021-03-09 00:00:07 -0300

    getplaneby type id

--- src/persistence/daos/DAOPlaneType.java
+++ src/persistence/daos/DAOPlaneType.java
@@ -1,7 +1,36 @@
 package persistence.daos;
 
+import logic.models.Game;
+import logic.models.Plane;
+import persistence.connection.IDBConnection;
 import persistence.daos.interfaces.IDAOPlaneType;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import exceptions.*;
 public class DAOPlaneType implements IDAOPlaneType {
+	
+	public Plane getPlaneByType(int planetypeId, IDBConnection icon) throws PersistenceException{
+		Plane plane = null;
+Connection con = icon.getConnection();
+		
+		try {
+			PreparedStatement pstmt = con.prepareStatement("select id,nombre,velocidad,consumo_combustible,blindaje,poder_fuego from AVIONES_CONFIG where id = ?");
+			pstmt.setInt(1, planetypeId);
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				plane = new Plane(rs.getDouble("velocidad"),rs.getDouble("blindaje"),rs.getDouble("poder_fuego"));
+			}
+			rs.close();
+			pstmt.close();
+		} catch (SQLException e) {
+			throw new PersistenceException("Error SQL: " + e.getMessage());
+		}
+		return plane;
+	}
+	
 
 }
--- src/persistence/daos/interfaces/IDAOPlaneType.java
+++ src/persistence/daos/interfaces/IDAOPlaneType.java
@@ -1,6 +1,10 @@
 package persistence.daos.interfaces;
 
+import exceptions.PersistenceException;
+import logic.models.Plane;
+import persistence.connection.IDBConnection;
+
 public interface IDAOPlaneType {
 	
-
+	Plane getPlaneByType(int planetype, IDBConnection icon) throws PersistenceException;
 }
