2adfc6e6c1056f738d8ff952768069d8c6bd1f6f
Santiago Fontana <santiagofontana7@gmail.com>
2021-03-16 11:00:36 -0300

    Datos reales de tipo de aviones/artillerias en BD

--- src/persistence/connection/config/insert1.sql
+++ src/persistence/connection/config/insert1.sql
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`,VOLANDO)VALUES(1,1,1,1,90,150,200,0);
 																													  
 INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`,VOLANDO)VALUES(2,1,1,1,270,850,200,0);
@@ -15,27 +14,6 @@ INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_A
 INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`,VOLANDO)VALUES(1,4,1,1,90,150,500,0);
 																													  
 INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`,VOLANDO)VALUES(2,4,1,1,270,850,500,0);
-=======
-INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`)VALUES(1,1,1,1,90,175,200);
-															                                        
-INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`)VALUES(2,1,1,1,270,825,200);
-															                                         
-INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`)VALUES(1,2,1,1,90,175,300);
-															                                          
-INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`)VALUES(2,2,1,1,270,825,300);
-															                                          
-INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`)VALUES(1,3,1,1,90,175,400);
-															                                           
-INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`)VALUES(2,3,1,1,270,825,400);
-															                                           
-INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`)VALUES(1,4,1,1,90,175,500);
-															                                           
-INSERT INTO `gameproject`.`aviones_config`(`BANDO`,NRO_AVION,TIENE_BOMBA,VUELO_ALTO,`ANGULO`,`POSICION_X`,`POSICION_Y`)VALUES(2,4,1,1,270,825,500);
-
-
-select * from aviones_config;
-
->>>>>>> 5bf910a256f8c62a5d2cc55ebdeba966e9d0c20b
 
 
 select * from aviones_config;
